775e46862c075c86164dc7bed0443bce6dba44a3
tales-andre <129626867+tales-andre@users.noreply.github.com>
2026-02-07 10:01:27 -0300

    template sqs

---
+++ skeleton-sqs-sns/main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region = "${{ values.region }}"
+}
+
+resource "aws_sns_topic" "this" {
+  name = "${{ values.component_id }}-topic"
+}
+
+resource "aws_sqs_queue" "this" {
+  name = "${{ values.component_id }}-queue"
+}
+
+resource "aws_sns_topic_subscription" "this" {
+  topic_arn = aws_sns_topic.this.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.this.arn
+}
+
+resource "aws_sqs_queue_policy" "this" {
+  queue_url = aws_sqs_queue.this.id
+  policy    = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqs-sns-policy",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "sns.amazonaws.com"
+      },
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.this.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_sns_topic.this.arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
